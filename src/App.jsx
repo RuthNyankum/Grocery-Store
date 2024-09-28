468909e1a94707cf30602b9463f8aef1f8754539
RuthNyankum <rnyankum99@gmail.com>
2024-09-28 22:39:47 +0000

    project initial

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Home from './pages/home';
+import Products from './pages/products';
+import Cart from './pages/cart';
+import Blog from './pages/blog';
+import Contact from './pages/contact';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: 'products',
+      element: <Products />,
+    },
+    {
+      path: 'cart',
+      element: <Cart />,
+    },
+    {
+      path: 'blog',
+      element: <Blog />,
+    },
+    {
+      path: 'contact-us',
+      element: <Contact />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
